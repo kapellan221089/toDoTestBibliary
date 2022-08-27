28c0b122e50616140efc0fb2a773b001f6bdbc24
kapellan221089 <serg221089@gmail.com>
2022-08-27 19:44:45 +0300

    fix4

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import VueSocketIO from 'vue-socket.io';
-const PORT =  process.env.PORT || 'http://localhost:3025'
 
 export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
-      //connection: 'http://localhost:3025',
+      connection: 'http://localhost:3025',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
