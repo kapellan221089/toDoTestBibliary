d583a6740bacd99ad7b4d66eb9f73c24dff05868
kapellan221089 <serg221089@gmail.com>
2022-08-27 20:39:30 +0300

    1

--- plugins/socket.js
+++ plugins/socket.js
@@ -6,7 +6,7 @@ export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: true,
-      connection: SocketIO('http://localhost:3025'),
+      connection: 'http://localhost:3025',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
